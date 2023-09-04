fce3a4cbfa03a9a99e90349499b08acc2c0aee9d
Sorasak-dev <122780126+Sorasak-dev@users.noreply.github.com>
2023-09-04 09:22:01 +0000

    lab03

--- src/test/java/th/mfu/TestBMICalculatorIT.java
+++ src/test/java/th/mfu/TestBMICalculatorIT.java
@@ -24,6 +24,7 @@ public class TestBMICalculatorIT {
     public static void createClient() {
         // Use ClientBuilder to create a new client that can be used to create
         // connections to the Web service.
+
         client = ClientBuilder.newClient();
     }
 
@@ -68,5 +69,26 @@ public class TestBMICalculatorIT {
             _logger.info("IT1 test passed");
         }
     }
+    @Test
+    public void testCaclulate3() {
+
+        // Make a HTTP GET request to retrieve the last created Parolee.
+        try (Response response = client.target(WEB_URI+"?weight=50&height=1.9").request().get()) {
+
+            // Check that the HTTP response code is 200 OK.
+            int responseCode = response.getStatus();
+            assertEquals(200, responseCode);
+
+            String jsonResponse = response.readEntity(String.class);
+            assertThat(jsonResponse, CoreMatchers.containsString("Result is 14"));
+
+            assertThat(jsonResponse, CoreMatchers.containsString("underweight"));
+            _logger.info("IT1 test passed");
+        }
+    }
+
+
+
+
 
 }
